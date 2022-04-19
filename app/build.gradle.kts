8cf3a7b44daee70d77c93cea2261eb032bf55b5e
Anders Østby <anders.ostby@nav.no>
2022-04-19 09:54:42 +0200

    Bumper kotlin, gradle og andre avhengigheter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,12 @@ dependencies {
     implementation("io.ktor:ktor-server-core:1.6.8")
     implementation("io.ktor:ktor-server-netty:1.6.8")
     implementation("io.ktor:ktor-client-jackson:1.6.8")
+    constraints {
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2") {
+            because("2.13.2 vulnerability")
+        }
+    }
+
 
     implementation("io.ktor:ktor-client-core:1.6.8")
     implementation("io.ktor:ktor-client-cio:1.6.8")
@@ -21,21 +27,21 @@ dependencies {
     implementation("io.ktor:ktor-client-logging:1.6.8")
 
     implementation("io.ktor:ktor-metrics-micrometer:1.6.8")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.4")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
 
     implementation("no.nav.aap.avro:sokere:3.0.2")
     implementation("no.nav.aap.avro:manuell:0.0.3")
     implementation("no.nav.aap.avro:inntekter:0.0.2")
     implementation("no.nav.aap.avro:medlem:1.1.6")
 
-    implementation("no.nav.security:token-validation-ktor:2.0.13")
-    implementation("no.nav.security:token-client-core:2.0.13")
+    implementation("no.nav.security:token-validation-ktor:2.0.14")
+    implementation("no.nav.security:token-client-core:2.0.14")
 
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.0.3")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.1.1")
 
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("io.ktor:ktor-jackson:1.6.8")
-    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.0.1")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.1.1")
 
     implementation("org.apache.kafka:kafka-clients:3.1.0")
     implementation("org.apache.kafka:kafka-streams:3.1.0")
@@ -44,7 +50,7 @@ dependencies {
             because("Mac M1")
         }
     }
-    implementation("io.confluent:kafka-streams-avro-serde:7.0.1") {
+    implementation("io.confluent:kafka-streams-avro-serde:7.1.0") {
         exclude("org.apache.kafka", "kafka-clients")
     }
 
@@ -53,7 +59,7 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("io.ktor:ktor-server-test-host:1.6.8")
-    testImplementation("no.nav.security:mock-oauth2-server:0.4.3")
+    testImplementation("no.nav.security:mock-oauth2-server:0.4.4")
     // used to override env var runtime
     testImplementation("uk.org.webcompere:system-stubs-jupiter:2.0.1")
     testImplementation("org.apache.kafka:kafka-streams-test-utils:3.1.0")
