b7c629c58db3c8d7d92320394f723abf7d47f957
Anders Østby <anders.ostby@nav.no>
2022-04-25 14:41:42 +0200

    Beholder test fra aktivitetstidslinje til oppdrag

--- domene/src/test/kotlin/no/nav/aap/domene/utbetaling/aktivitetstidslinje/FraAktivitetstidslinjeTilOppdragBuilderTest.kt
+++ domene/src/test/kotlin/no/nav/aap/domene/utbetaling/aktivitetstidslinje/FraAktivitetstidslinjeTilOppdragBuilderTest.kt
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-internal class OppdragBuilderTest {
+internal class FraAktivitetstidslinjeTilOppdragBuilderTest {
 
     @BeforeEach
     fun beforeEach() {
