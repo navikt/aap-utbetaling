76b59617025ee45c912540d11470ef932fd07843
Anders Østby <anders.ostby@nav.no>
2022-04-25 14:31:34 +0200

    Visitor for Utbetalingstidslinje
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>

--- domene/src/main/kotlin/no/nav/aap/domene/utbetaling/utbetalingstidslinje/Utbetalingstidslinje.kt
+++ domene/src/main/kotlin/no/nav/aap/domene/utbetaling/utbetalingstidslinje/Utbetalingstidslinje.kt
@@ -4,4 +4,15 @@ internal class Utbetalingstidslinje(
     dager: List<Utbetalingsdag>
 ) {
     private val dager = dager.toMutableList()
+
+    internal fun accept(visitor: UtbetalingstidslinjeVisitor){
+        visitor.preVisitUtbetalingstidslinje(this)
+        dager.forEach { it.accept(visitor) }
+        visitor.postVisitUtbetalingstidslinje(this)
+    }
+}
+
+internal interface UtbetalingstidslinjeVisitor : UtbetalingsdagVisitor {
+    fun preVisitUtbetalingstidslinje(tidslinje: Utbetalingstidslinje) {}
+    fun postVisitUtbetalingstidslinje(tidslinje: Utbetalingstidslinje) {}
 }
