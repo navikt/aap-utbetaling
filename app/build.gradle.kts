f8eb375bc11e3609deae433013a9f235c16ec646
Anders Østby <anders.ostby@nav.no>
2022-06-23 08:47:36 +0200

    Bumper avhengigheter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,14 +10,14 @@ application {
 dependencies {
     implementation(project(":domene"))
 
-    implementation("com.github.navikt.aap-libs:ktor-utils:2.0.1")
-    implementation("com.github.navikt.aap-libs:kafka:2.0.1")
+    implementation("com.github.navikt.aap-libs:ktor-utils:2.0.6")
+    implementation("com.github.navikt.aap-libs:kafka:2.0.6")
 
     implementation("io.ktor:ktor-server-core:2.0.2")
     implementation("io.ktor:ktor-server-netty:2.0.2")
 
     implementation("io.ktor:ktor-server-metrics-micrometer:2.0.2")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.9.0")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.1")
 
     implementation("ch.qos.logback:logback-classic:1.2.11")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.2")
@@ -27,7 +27,7 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
 
     testImplementation(kotlin("test"))
-    testImplementation("com.github.navikt.aap-libs:kafka-test:2.0.1")
+    testImplementation("com.github.navikt.aap-libs:kafka-test:2.0.6")
     testImplementation("io.ktor:ktor-server-test-host:2.0.2")
 }
 
