78d392297a3758065925dc08c5abb60862e3ed5e
Anders Østby <anders.ostby@nav.no>
2022-07-26 09:46:13 +0200

    Bumper avhengigheter Kotlin til 1.7.10 Gradle til 7.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-val aapLibVersion = "2.1.4"
+val aapLibVersion = "2.1.11"
 val ktorVersion = "2.0.3"
 
 plugins {
@@ -20,7 +20,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.9.1")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.2")
 
     implementation("ch.qos.logback:logback-classic:1.2.11")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.2")
