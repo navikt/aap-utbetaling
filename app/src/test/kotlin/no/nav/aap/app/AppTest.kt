3f0578806abbf1dccddb5cf8b2cd0316c3bd0df0
Anders Østby <anders.ostby@nav.no>
2022-10-12 12:48:28 +0200

    Bumper aap-libs til 3.5.2

--- app/src/test/kotlin/no/nav/aap/app/AppTest.kt
+++ app/src/test/kotlin/no/nav/aap/app/AppTest.kt
@@ -7,7 +7,6 @@ import no.nav.aap.domene.utbetaling.dto.DtoLøsning
 import no.nav.aap.domene.utbetaling.dto.DtoLøsningBarn
 import no.nav.aap.domene.utbetaling.dto.DtoMeldepliktshendelse
 import no.nav.aap.dto.kafka.IverksettVedtakKafkaDto
-import no.nav.aap.kafka.streams.test.readAndAssert
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.util.*
@@ -16,8 +15,8 @@ internal class AppTest {
 
     @Test
     fun `Innsendt vedtak oppretter en mottaker`() = withTestApp { mocks ->
-        val vedtakTopic = mocks.kafka.inputTopic(Topics.vedtak)
-        val mottakTopic = mocks.kafka.outputTopic(Topics.mottakere)
+        val vedtakTopic = mocks.kafka.testTopic(Topics.vedtak)
+        val mottakTopic = mocks.kafka.testTopic(Topics.mottakere)
 
         vedtakTopic.produce("123") {
             IverksettVedtakKafkaDto(
@@ -30,15 +29,15 @@ internal class AppTest {
             )
         }
 
-        mottakTopic.readAndAssert().hasValuesForPredicate("123", 1) {
+        mottakTopic.assertThat().hasValuesForPredicate("123", 1) {
             it.personident == "123"
         }
     }
 
     @Test
     fun `Innsendt vedtak oppretter en mottaker med vedtakshistorikk`() = withTestApp { mocks ->
-        val vedtakTopic = mocks.kafka.inputTopic(Topics.vedtak)
-        val mottakTopic = mocks.kafka.outputTopic(Topics.mottakere)
+        val vedtakTopic = mocks.kafka.testTopic(Topics.vedtak)
+        val mottakTopic = mocks.kafka.testTopic(Topics.mottakere)
         val vedtaksid = UUID.randomUUID()
         vedtakTopic.produce("123") {
             IverksettVedtakKafkaDto(
@@ -51,16 +50,16 @@ internal class AppTest {
             )
         }
 
-        mottakTopic.readAndAssert().hasValuesForPredicate("123", 1) {
+        mottakTopic.assertThat().hasValuesForPredicate("123", 1) {
             it.vedtakshistorikk.size == 1 && it.vedtakshistorikk[0].vedtaksid == vedtaksid
         }
     }
 
     @Test
     fun `Innsendt meldepliktshendelse oppretter en periode i aktivitetstidslinja`() = withTestApp { mocks ->
-        val vedtakTopic = mocks.kafka.inputTopic(Topics.vedtak)
-        val mottakTopic = mocks.kafka.outputTopic(Topics.mottakere)
-        val meldepliktTopic = mocks.kafka.inputTopic(Topics.meldeplikt)
+        val vedtakTopic = mocks.kafka.testTopic(Topics.vedtak)
+        val mottakTopic = mocks.kafka.testTopic(Topics.mottakere)
+        val meldepliktTopic = mocks.kafka.testTopic(Topics.meldeplikt)
         val vedtaksid = UUID.randomUUID()
         vedtakTopic.produce("123") {
             IverksettVedtakKafkaDto(
@@ -85,20 +84,20 @@ internal class AppTest {
             )
         }
 
-        mottakTopic.readAndAssert().hasValuesForPredicate("123", 1) {
+        mottakTopic.assertThat().hasValuesForPredicate("123", 1) {
             it.aktivitetstidslinje.size == 1
         }
     }
 
     @Test
     fun `Innsendt løsning beregner en utbetaling`() = withTestApp { mocks ->
-        val vedtakInputTopic = mocks.kafka.inputTopic(Topics.vedtak)
-        val mottakOutputTopic = mocks.kafka.outputTopic(Topics.mottakere)
-        val meldepliktInputTopic = mocks.kafka.inputTopic(Topics.meldeplikt)
-        val løsningInputTopic = mocks.kafka.inputTopic(Topics.utbetalingsbehov)
+        val vedtakTopic = mocks.kafka.testTopic(Topics.vedtak)
+        val mottakTopic = mocks.kafka.testTopic(Topics.mottakere)
+        val meldepliktTopic = mocks.kafka.testTopic(Topics.meldeplikt)
+        val løsningTopic = mocks.kafka.testTopic(Topics.utbetalingsbehov)
         val løsningOutputTopic = mocks.kafka.outputTopic(Topics.utbetalingsbehov)
         val vedtaksid = UUID.randomUUID()
-        vedtakInputTopic.produce("123") {
+        vedtakTopic.produce("123") {
             IverksettVedtakKafkaDto(
                 vedtaksid = vedtaksid,
                 innvilget = true,
@@ -109,7 +108,7 @@ internal class AppTest {
             )
         }
 
-        meldepliktInputTopic.produce("123") {
+        meldepliktTopic.produce("123") {
             DtoMeldepliktshendelse(
                 aktivitetPerDag = listOf(
                     DtoAkivitetPerDag(
@@ -122,7 +121,7 @@ internal class AppTest {
         }
 
         val behov = løsningOutputTopic.readValue()
-        løsningInputTopic.produce("123") {
+        løsningTopic.produce("123") {
             behov.copy(
                 response = KafkaUtbetalingsbehovWrapper.KafkaUtbetalingsbehov.Response(
                     DtoLøsning(
@@ -132,16 +131,16 @@ internal class AppTest {
             )
         }
 
-        mottakOutputTopic.readAndAssert().hasValuesForPredicate("123", 1) {
+        mottakTopic.assertThat().hasValuesForPredicate("123", 1) {
             it.utbetalingstidslinjehistorikk.size == 1 && it.oppdragshistorikk.size == 1
         }
     }
 
     @Test
     fun `Meldepliktshendelse trigger behov for barn`() = withTestApp { mocks ->
-        val vedtakTopic = mocks.kafka.inputTopic(Topics.vedtak)
-        val meldepliktTopic = mocks.kafka.inputTopic(Topics.meldeplikt)
-        val utbetalingsbehovOutputTopic = mocks.kafka.outputTopic(Topics.utbetalingsbehov)
+        val vedtakTopic = mocks.kafka.testTopic(Topics.vedtak)
+        val meldepliktTopic = mocks.kafka.testTopic(Topics.meldeplikt)
+        val utbetalingsbehovOutputTopic = mocks.kafka.testTopic(Topics.utbetalingsbehov)
         val vedtaksid = UUID.randomUUID()
         vedtakTopic.produce("123") {
             IverksettVedtakKafkaDto(
@@ -166,7 +165,7 @@ internal class AppTest {
             )
         }
 
-        utbetalingsbehovOutputTopic.readAndAssert().hasValuesForPredicate("123", 1) {
+        utbetalingsbehovOutputTopic.assertThat().hasValuesForPredicate("123", 1) {
             it.request.ident == "123"
         }
     }
