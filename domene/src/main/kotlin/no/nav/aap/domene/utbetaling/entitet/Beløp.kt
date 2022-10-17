74d5ac0e95be156e3bad3647334d72ec81526879
Anders Østby <anders.ostby@nav.no>
2022-10-17 12:57:11 +0200

    Bumper og bruker StateStoreMigrator fra libs

--- domene/src/main/kotlin/no/nav/aap/domene/utbetaling/entitet/Beløp.kt
+++ domene/src/main/kotlin/no/nav/aap/domene/utbetaling/entitet/Beløp.kt
@@ -3,7 +3,7 @@ package no.nav.aap.domene.utbetaling.entitet
 import kotlin.math.round
 import kotlin.math.roundToInt
 
-internal class Beløp(verdi: Number) : Comparable<Beløp> {
+internal class Beløp private constructor(verdi: Number) : Comparable<Beløp> {
 
     private val verdi: Double = round(verdi.toDouble() * 100) / 100
 
