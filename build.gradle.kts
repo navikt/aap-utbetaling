f3440b0232abf9aa88a0e058b8c92574391e7dcc
Anders Østby <anders.ostby@nav.no>
2022-11-21 09:21:51 +0100

    Bumper avhengigheter
    * Kotlin: 1.7.21
    * Ktor plugin: 2.1.3
    * gradle-build-action: 2.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    id("io.ktor.plugin") version "2.1.2" apply false
+    kotlin("jvm") version "1.7.21"
+    id("io.ktor.plugin") version "2.1.3" apply false
 }
 
 allprojects {
